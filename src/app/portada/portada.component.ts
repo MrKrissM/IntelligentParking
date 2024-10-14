860819a83f98a89e2a11b068b6f2e800353cb1a0
Your Name <you@example.com>
2024-10-14 20:35:21 -0300

    Portada

---
+++ src/app/portada/portada.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-portada',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './portada.component.html',
+  styleUrls: ['./portada.component.css']
+})
+export class PortadaComponent {
+  
+ }
