cd3014d80340f198ba1c29ce51b7f88db64f91ee
MrKrissM <cristian.miranda2023@twk.cl>
2024-11-25 18:45:49 -0300

    agregado vehiculos

--- src/app/vehicles/vehicles.component.ts
+++ src/app/vehicles/vehicles.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { VehicleService } from '../services/vehicle/vehicle.service';
 
 @Component({
   selector: 'app-vehicles',
@@ -17,4 +18,27 @@ export class VehiclesComponent {
     { plate: 'JKL321', brand: 'Chevrolet', model: 'Spark', color: 'Blanco', status: 'No Estacionado' },
     { plate: 'QWE987', brand: 'Nissan', model: 'Versa', color: 'Gris', status: 'Estacionado' }
   ];
+
+  constructor(private vehicleService: VehicleService) {}
+  
+  addVehicle(): void {
+    const plateInput = (document.getElementById('plate') as HTMLInputElement).value;
+    const modelInput = (document.getElementById('model') as HTMLInputElement).value || null;
+    const brandInput = (document.getElementById('brand') as HTMLInputElement).value || null;
+    const colorInput = (document.getElementById('color') as HTMLInputElement).value || null;
+
+    const newVehicle = {
+      plate: plateInput,
+      model: modelInput,
+      brand: brandInput,
+      color: colorInput
+    };
+
+    this.vehicleService.createVehicle(newVehicle).subscribe(response => {
+      // Manejar la respuesta, como añadir el nuevo vehículo a la lista
+      this.vehicles.push(response);
+    }, error => {
+      console.error('Error al agregar vehículo:', error);
+    });
+  }
 }
